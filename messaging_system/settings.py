c9ef23909bbc722b840e4f3f062e7a2c2c13d68e
barlavi1 <barannabel@gmail.com>
2024-02-20 12:27:55 +0200

    change DB settings to remove Heroku adjustment

--- messaging_system/settings.py
+++ messaging_system/settings.py
@@ -12,8 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
-import dj_database_url
-import django_heroku
+#import dj_database_url
+#import django_heroku
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -86,20 +86,20 @@ WSGI_APPLICATION = "messaging_system.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-#DATABASES = {
-#    "default": {
-#        "ENGINE": "django.db.backends.mysql",
-#        "NAME": "msg_system_db",
-#        'USER': 'barl',
-#        'PASSWORD': 'Pa$$w0rd',
-#        'HOST': '127.0.0.1',
-#        'PORT': '3306'
-#    }
-#}
-
 DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('mysql://barl:Pa$$w0rd@127.0.0.1.jawsdb.com:3306/msg_system_db'), engine='django.db.backends.mysql')
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "msg_system_db",
+        'USER': 'barl',
+        'PASSWORD': 'Pa$$w0rd',
+        'HOST': '127.0.0.1',
+        'PORT': '3306'
+    }
 }
+
+#DATABASES = {
+#        'default': dj_database_url.parse(os.environ.get('mysql://barl:Pa$$w0rd@127.0.0.1.jawsdb.com:3306/msg_system_db'), engine='django.db.backends.mysql')
+#}
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
@@ -166,5 +166,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-django_heroku.settings(locals())
+#DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+#django_heroku.settings(locals())
